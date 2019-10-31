8e945f2c00098af7c25cc63c54b1229b750c6e07
clement <clement@circleedges.com>
2019-10-31 16:29:02 +0200

    feature(records):add records page [Finishes #169390297]

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -2,3 +2,4 @@ const userCreated = () => window.location = "../pages/login.html";
 const signin = () => window.location = "../pages/myRecords.html";
 const forgetPassword = () => window.location = "../pages/passReseted.html";
 const resetPassword = () => window.location = "../pages/login.html";
+const readrecord = () => window.location = "../pages/viewRecord.html";
