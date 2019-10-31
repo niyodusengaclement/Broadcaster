8337f4d56738db3f6bfd59f4289fce6a449599eb
clement <clement@circleedges.com>
2019-10-31 12:08:45 +0200

    feature(new-password):add new-password page [Finishes #169476300]

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -1,3 +1,4 @@
 const userCreated = () => window.location = "../pages/login.html";
 const signin = () => window.location = "../pages/myRecords.html";
 const forgetPassword = () => window.location = "../pages/passReseted.html";
+const resetPassword = () => window.location = "../pages/login.html";
