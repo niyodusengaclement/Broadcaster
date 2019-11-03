ae153e6628b336e9e32093c5383127369c905022
NIYODUSENGA Clement <clementmistico@gmail.com>
2019-11-03 23:55:09 +0200

    feature(administrator):add administrator pages [Finishes #169520231]

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -1,13 +1,16 @@
-const userCreated = () => window.location = "../pages/login.html";
-const signin = () => window.location = "../pages/myRecords.html";
-const forgetPassword = () => window.location = "../pages/passReseted.html";
-const resetPassword = () => window.location = "../pages/login.html";
-const readrecord = () => window.location = "../pages/viewRecord.html";
-const createRecord = () => window.location = "../pages/myRecords.html";
-const editRecord = () => window.location = "../pages/records.html";
+const userCreated = () => window.location = '../pages/login.html';
+const signin = () => window.location = '../pages/myRecords.html';
+const forgetPassword = () => window.location = '../pages/passReseted.html';
+const resetPassword = () => window.location = '../pages/login.html';
+const readrecord = () => window.location = '../pages/viewRecord.html';
+const createRecord = () => window.location = '../pages/myRecords.html';
+const editRecord = () => window.location = '../pages/records.html';
 const deleterecord = (title) => {
     const ok = window.confirm(`Are sure, you want to delete this report? \n ${title}`);
-    if ( ok ){
-        location = "../pages/recordDeleted.html";
+    if (ok) {
+        location = '../pages/recordDeleted.html';
     }
 }
+
+const adminSignin = () => window.location ='../admin/allRecords.html';
+const adminReadrecord = () => window.location = "../admin/viewRecord.html";
