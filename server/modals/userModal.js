c9df51306ef40cd58b5c0ba1b4e4b2c16bafbbdd
NIYODUSENGA Clement <clementmistico@gmail.com>
2019-11-08 17:38:00 +0200

    feature(specific-record):add specific red-flag endpoint [Finishes #169648790]

--- server/modals/userModal.js
+++ server/modals/userModal.js
@@ -1,8 +1,10 @@
 import users from '../asset/users';
+import reportData from '../asset/report';
 
 class UserModal {
   constructor() {
     this.user = users;
+    this.report = reportData;
   }
 
   findUser(email) {
@@ -11,6 +13,10 @@ class UserModal {
 
   addUser(data) {
     return this.user.push(data);
+  }
+
+  findReport(id) {
+    return this.report.find((data) => data.id === id);
   }
 }
 export default new UserModal();
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,13 @@ import signup from '../controllers/signup';
 import signin from '../controllers/signin';
 import userAuthentication from '../middleware/auth';
 import allReports from '../controllers/AllReports';
+import oneRecord from '../controllers/oneRecord';
 
 const routes = express.Router();
 
 routes.post('/auth/signup', signup);
 routes.post('/auth/signin', signin);
 routes.get('/red-flags', userAuthentication, allReports);
+routes.get('/red-flags/:redFlagId', userAuthentication, oneRecord);
 
 export default routes;
