8956f5eb58a9b28d2fa6057efa53315cc7cad8ac
NIYODUSENGA Clement <clementmistico@gmail.com>
2019-11-10 00:49:03 +0200

    feature(delete):add delete endpoint [Finishes #169650694]

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ import allReports from '../controllers/AllReports';
 import oneRecord from '../controllers/oneRecord';
 import changeLocation from '../controllers/changeLocation';
 import editComment from '../controllers/editComment';
+import deleteRecord from '../controllers/deleteRecord';
 
 const routes = express.Router();
 
@@ -15,5 +16,6 @@ routes.get('/red-flags', userAuthentication, allReports);
 routes.get('/red-flags/:redFlagId', userAuthentication, oneRecord);
 routes.patch('/red-flags/:red_Flag_Id/location', userAuthentication, changeLocation);
 routes.patch('/red-flags/:red_Flag_Id/comment', userAuthentication, editComment);
+routes.delete('/red-flags/:red_Flag_Id', userAuthentication, deleteRecord);
 
 export default routes;
