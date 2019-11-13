21224f78b775e5f70490bfbfd8d1a96f06517d29
NIYODUSENGA Clement <clementmistico@gmail.com>
2019-11-13 17:35:25 +0200

    chore(documentation):add swagger documentation [Finishes #169690466]

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import fileupload from 'express-fileupload';
+import { serve, setup } from 'swagger-ui-express';
 import env from 'dotenv';
 import routes from './routes';
+import swaggerDoc from '../swagger.json';
 
 env.config();
 const app = express();
@@ -11,6 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(fileupload({ createParentPath: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api-docs', serve, setup(swaggerDoc));
 app.use('/api/v1', routes);
 
 app.listen(port, () => {
