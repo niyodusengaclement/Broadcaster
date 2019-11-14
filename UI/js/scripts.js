36ec90564b56524f50d47253be5acf04b60e1bcb
NIYODUSENGA Clement <clementmistico@gmail.com>
2019-11-14 21:07:18 +0200

    bug(contents):add clear contents and remove unclear ones [Finishes #169740334]

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -1,10 +1,34 @@
-const userCreated = () => { window.location = '../pages/login.html'; };
-const signin = () => { window.location = '../pages/myRecords.html'; };
-const forgetPassword = () => { window.location = '../pages/passReseted.html'; };
-const resetPassword = () => { window.location = '../pages/login.html'; };
+
+function match() {
+  document.getElementById('confPasswordRes').innerHTML = 'Password doesn\'t match';
+  document.getElementById('confPassword').style.border = '1px solid red';
+}
+
+const userCreated = () => {
+  const signupForm = document.createAccountForm;
+  const pass = document.createAccountForm.password.value;
+  const confirmPass = document.createAccountForm.confPassword.value;
+  if (signupForm.checkValidity() === false) {
+    return false;
+  }
+  if (confirmPass !== pass) {
+    match();
+    return false;
+  }
+};
+
+const resetPassword = () => {
+  const password = document.resetForm.password.value;
+  const confirmPass = document.resetForm.confPassword.value;
+
+  if (confirmPass !== password) {
+    match();
+    return false;
+  }
+};
+
 const readrecord = () => { window.location = '../pages/viewRecord.html'; };
-const createRecord = () => { window.location = '../pages/myRecords.html'; };
-const editRecord = () => { window.location = '../pages/records.html'; };
+
 const deleterecord = (title) => {
   const ok = window.confirm(`Are sure, you want to delete this report? \n ${title}`);
   if (ok) {
@@ -12,5 +36,4 @@ const deleterecord = (title) => {
   }
 };
 
-const adminSignin = () => { window.location = '../admin/allRecords.html'; };
 const adminReadrecord = () => { window.location = '../admin/viewRecord.html'; };
