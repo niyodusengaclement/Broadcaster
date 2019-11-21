e5c9936b42363075f21d29b1fc8f8b8a67c6a7b2
NIYODUSENGA Clement <clementmistico@gmail.com>
2019-11-21 17:57:51 +0200

    chore(refactoring):improve test of uploading files [Finishes #169770248]

--- test/reports/createReport.test.js
+++ test/reports/createReport.test.js
@@ -42,6 +42,64 @@ const createReportTest = () => {
         done();
       });
   });
+  it('User should be able to upload single video', (done) => {
+    request(app)
+      .post('/api/v1/red-flags')
+      .set(reportData.validToken)
+      .field('title', 'my title')
+      .field('type', 'my title')
+      .field('comment', 'my title')
+      .attach('videos', './test/asset/flag.mp4', 'flag.mp4')
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        done();
+      });
+  });
+  it('User should be able to upload multiple videos', (done) => {
+    request(app)
+      .post('/api/v1/red-flags')
+      .set(reportData.validToken)
+      .field('title', 'my title')
+      .field('type', 'my title')
+      .field('comment', 'my title')
+      .attach('videos', './test/asset/flag.mp4', 'flag.mp4')
+      .attach('videos', './test/asset/red.mp4', 'red.mp4')
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        done();
+      });
+  });
+  it('User should be able to upload single image', (done) => {
+    request(app)
+      .post('/api/v1/red-flags')
+      .set(reportData.validToken)
+      .field('title', 'Bad roads in countryside')
+      .field('type', 'Intervention')
+      .field('comment', 'I would like to let you know about destroyed roads in countryside')
+      .attach('images', './test/asset/epic.png', 'epic.png')
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body).to.have.a.property('status', 201);
+        expect(res.body).to.have.a.property('data');
+        done();
+      });
+  });
+  it('User should be able to upload multiple images', (done) => {
+    request(app)
+      .post('/api/v1/red-flags')
+      .set(reportData.validToken)
+      .field('title', 'Bad roads in countryside')
+      .field('type', 'Intervention')
+      .field('comment', 'I would like to let you know about destroyed roads in countryside')
+      .attach('images', './test/asset/reset.png', 'reset.png')
+      .attach('images', './test/asset/epic.png', 'epic.png')
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body).to.have.a.property('status', 201);
+        expect(res.body).to.have.a.property('data');
+        done();
+      });
+  });
   it('User should create report if there is no mistake', (done) => {
     request(app)
       .post('/api/v1/red-flags')
