a91e77a06dc88bf5e5940fcc558c3078f97686e7
NIYODUSENGA Clement <clementmistico@gmail.com>
2019-12-02 22:30:23 +0200

    chore(database):setup database and travis [Finishes #170044124]

---
+++ server/modals/v2/db.js
@@ -0,0 +1,17 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const con = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : process.env.LOCAL_DB_URL;
+
+const pool = new Pool(con);
+export default {
+  query(text, params) {
+    try {
+      return pool.query(text, params);
+    } catch (err) {
+      return err;
+    }
+  },
+};
